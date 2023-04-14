725378918358375c3cc92250e5e1cbc0cf54904c
szemanlevi <szemanlevente@gmail.com>
2023-04-14 09:25:18 +0200

    first commit

---
+++ src/main/java/com/example/expense_tracker_szemanlevi/ExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.expense_tracker_szemanlevi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerSzemanleviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTrackerSzemanleviApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/expense_tracker_szemanlevi/ExpenseTrackerSzemanleviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expense_tracker_szemanlevi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerSzemanleviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
